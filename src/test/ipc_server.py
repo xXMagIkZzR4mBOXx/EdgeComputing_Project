35257790a0e3cc0ed11b3553cccb5b8e04624de7
Emile Aydar <114087019+xXMagIkZzR4mBOXx@users.noreply.github.com>
2024-04-15 02:03:57 +0200

    Update ipc_server.py

--- src/test/ipc_server.py
+++ src/test/ipc_server.py
@@ -3,8 +3,8 @@
 
 import socket
 
-HOST = '0.0.0.0'  # listen on all network interfaces
-PORT = 9898        # Port to listen on (non-privileged ports are > 1023)
+HOST = '0.0.0.0'
+PORT = 9898 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
